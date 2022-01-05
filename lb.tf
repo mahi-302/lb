09e41af729aaf4e10a13aa34ea7f6e1cf8dec8b1
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-05 19:12:27 +0530

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -2,30 +2,30 @@ provider "aws" {
   profile = "default"
   region = "us-east-2"
 }
-resource "aws_lb" "abcd" {
-  name               = "test-lb-tf1"
+resource "aws_lb" "terratasks" {
+  name               = "lbtf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["sg-0188068a44c521d97"]
-  subnets            = ["subnet-02ac0c9e52da309ab","subnet-08f3fd927977dc180",]
+  security_groups    = ["sg-03723ff32a5606bda"]
+  subnets            = ["subnet-0c9f6c09d2f62dc2d"]
 }
-resource "aws_lb_target_group" "abcd" {
-  name     = "tf-example-lb-tg"
+resource "aws_lb_target_group" "terratasks" {
+  name     = "lbtgtf"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "vpc-0a7387e5c61ec7456"
 }
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.abcd.arn
+  load_balancer_arn = aws_lb.terratasks.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.abcd.arn
+    target_group_arn = aws_lb_target_group.terratasks.arn
   }
 }
-resource "aws_lb_target_group_attachment" "abcd" {
-  target_group_arn = aws_lb_target_group.abcd.arn
-  target_id        = "i-03f429574f46d2e9f"
+resource "aws_lb_target_group_attachment" "terratasks" {
+  target_group_arn = aws_lb_target_group.terratasks.arn
+  target_id        = "i-0ae95d4c23948efa5"
   port             = 80
 }
